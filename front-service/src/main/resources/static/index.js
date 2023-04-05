99ce8c099135cbb71ab25282bc802528c1281f48
mka-Mikhail <mixailrojnow@yandex.ru>
2023-04-05 10:21:09 +0300

    распилили фронт на отдельные модули

--- front-service/src/main/resources/static/index.js
+++ front-service/src/main/resources/static/index.js
@@ -1,4 +1,49 @@
-angular.module('app', ['ngStorage']).controller('indexController', function ($scope, $http, $localStorage) {
+(function () {
+    angular
+        .module('market', ['ngRoute', 'ngStorage'])
+        .config(config)
+        .run(run);
+
+    function config($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'welcome/welcome.html',
+                controller: 'welcomeController'
+            })
+            .when('/store', {
+                templateUrl: 'store/store.html',
+                controller: 'storeController'
+            })
+            .when('/cart', {
+                templateUrl: 'cart/cart.html',
+                controller: 'cartController'
+            })
+            .when('/registration', {
+                templateUrl: 'registration/registration.html',
+                controller: 'registrationController'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    }
+    function run($rootScope, $http, $localStorage) {
+        if ($localStorage.webUser) {
+            try {
+                let jwt = $localStorage.webUser.token;
+                let payload = JSON.parse(atob(jwt.split('.')[1]));
+                let currentTime = parseInt(new Date().getTime() / 1000);
+                if (currentTime > payload.exp) {
+                    console.log("Token id expired!");
+                    delete $localStorage.webUser;
+                    $http.defaults.headers.common.Authorization = '';
+                }
+            } catch (e) {}
+            $http.defaults.headers.common.Authorization = 'Bearer ' + $localStorage.webUser.token;
+        }
+    }
+})();
+
+angular.module('market').controller('indexController', function ($scope, $http, $localStorage, $location, $rootScope) {
 
     $scope.tryToAuth = function () {
         $http.post('http://localhost:5555/auth/auth', $scope.user)
@@ -8,14 +53,16 @@ angular.module('app', ['ngStorage']).controller('indexController', function ($sc
                     $localStorage.webUser = {username: $scope.user.username, token: response.data.token};
                     $scope.user.username = null;
                     $scope.user.password = null;
+                    $location.path('/');
                 }
             }, function errorCallback(response) {
             });
     };
 
-    $scope.tryToLogout = function () {
+    $rootScope.tryToLogout = function () {
         $scope.clearUser();
         $scope.user = null;
+        $location.path('/');
     };
 
     $scope.clearUser = function () {
@@ -23,71 +70,11 @@ angular.module('app', ['ngStorage']).controller('indexController', function ($sc
         $http.defaults.headers.common.Authorization = '';
     };
 
-    $scope.isUserLoggedIn = function () {
+    $rootScope.isUserLoggedIn = function () {
         if ($localStorage.webUser) {
             return true;
         } else {
             return false;
         }
     };
-
-    if ($localStorage.webUser) {
-        try {
-            let jwt = $localStorage.webUser.token;
-            let payload = JSON.parse(atob(jwt.split('.')[1]));
-            let currentTime = parseInt(new Date().getTime() / 1000);
-            if (currentTime > payload.exp) {
-                console.log("Token id expired!");
-                delete $localStorage.webUser;
-                $http.defaults.headers.common.Authorization = '';
-            }
-        } catch (e) {}
-        $http.defaults.headers.common.Authorization = 'Bearer ' + $localStorage.webUser.token;
-    }
-
-    $scope.loadProducts = function () {
-        $http.get('http://localhost:5555/core/api/v1/products').then(function (response) {
-            $scope.productList = response.data;
-        });
-    };
-
-    $scope.createOrder = function () {
-        $http.post('http://localhost:5555/core/api/v1/orders').then(function (response) {
-            alert('Заказ оформлен');
-            $scope.loadCart();
-        });
-    };
-
-    $scope.showProductInfo = function (productId) {
-        $http.get('http://localhost:5555/core/api/v1/products/' + productId).then(function (response) {
-            alert(response.data.title);
-        });
-    };
-
-    $scope.loadCart = function () {
-        $http.get('http://localhost:5555/cart/api/v1/cart').then(function (response) {
-            $scope.cart = response.data;
-        });
-    };
-
-    $scope.addToCart = function (productId) {
-        $http.get('http://localhost:5555/cart/api/v1/cart/add/' + productId).then(function (response) {
-            $scope.loadCart();
-        });
-    };
-
-    $scope.removeFromCart = function (productId) {
-        $http.get('http://localhost:5555/cart/api/v1/cart/remove/' + productId).then(function (response) {
-            $scope.loadCart();
-        });
-    };
-
-    $scope.clearCart = function () {
-        $http.get('http://localhost:5555/cart/api/v1/cart/clear').then(function (response) {
-            $scope.loadCart();
-        });
-    };
-
-    $scope.loadProducts();
-    $scope.loadCart();
 })
